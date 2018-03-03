a13dc95d65c012bb409672637ce1526e56cb2d32
Xavier Reid <xreid@xreid-mac2.tsi.lan>
2018-03-03 11:05:05 -0800

    added logic to get user and tracks

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,53 @@ let router = express.Router();
 let soundcloud = require('node-soundcloud');
 
 let id = ''; // assign your client id
-let secret = ''; // asign your client secret
-let uri = '';
+let secret = ''; // asign your secret
+let uri = ''; // redirect uri (not used)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   soundcloud.init({id, secret, uri});
-  
-  soundcloud.get('/resolve?url=http://soundcloud.com/soulection', function(error, user) {
+  let username = req.query.username; // username passed from find user button
+  let style = "display:none"; // style for download button
+
+  if (username != undefined) {
+    getUser(username, res, (user, res) => {
+      console.log(user.id);
+      getTracks(user.id, res, (tracks, res) => {
+        res.render('index', {user, style: "display:inline"});        
+      });
+    });
+  } else {
+    res.render('index', {style});      
+  }
+});
+
+/*
+** Get user by username
+*/
+function getUser(username, res, callback) {
+  soundcloud.get(`/resolve?url=http://soundcloud.com/${username}`, function(error, user) {
+      if (error) {
+        console.log(error); // TODO: should show alert
+      } else {
+        console.log(user.permalink);
+        callback(user, res);
+      }
+    });
+}
+
+/*
+** Get tracks by user id
+*/
+function getTracks(userId, res, callback) {
+  soundcloud.get(`/users/${userId}/tracks`, function(error, tracks) {
     if (error) {
-      throw error;
+      console.log(error); // TODO: should show alert
     } else {
-      console.log('user retrieved:', user);
+      console.log(tracks);
+      callback(tracks, res);
     }
   });
-
-  res.render('index');
-});
+}
 
 module.exports = router;
